23e2198f4001bf756fece04c7674cb83097770b6
Jonathan Martens <jmartens8@yahoo.de>
2022-07-28 10:54:51 +0200

    home header img

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -10,20 +10,22 @@ export default function Home() {
     <>
         <MyNavbar />
         <header>
-           <div className="container">
+            <img src="images/pic-sharing.jpg" alt="sharing" />
+        </header>
+        <section>
+            <div className="container">
                 <Link to='/donate'>
-                    <img className="homeImg" src="images/pic-donation2.jpg" alt="donation" />
+                    <img className="selectionImg" src="images/pic-donation2.jpg" alt="donation" />
                     <h2 className="overlay">Donate Food</h2>
                 </Link>
-             
-           </div>
-           <div className="container">
+            </div>
+            <div className="container">
                 <Link to='/save-food'>
-                    <img className="homeImg" src="images/pic-familyDinner.jpg" alt="save food" />
+                    <img className="selectionImg" src="images/pic-familyDinner.jpg" alt="save food" />
                     <h2 className="overlay">Save Food</h2>
                 </Link>
-           </div>
-        </header>
+            </div>
+        </section>
     </>
     
         // <MyNavbar />
